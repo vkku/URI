f6b6747d473bb2e2bfd1c49891c0e49add9fe34a
vkku <kumar.vivekten@gmail.com>
2016-08-25 23:33:27 +0530

    URI 1035

---
+++ Untitled1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    char arr[10] = {0, [2] = 5};
+    printf("Number : %d",arr[2] - 0 + 5);
+    return 0;
+}
---
+++ selectionTest1_1035.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(b > c && d > a && (c + d) > (a + b))
+    {
+        if(c > 0 && d > 0)
+        {
+            if(a % 2 == 0)
+            {
+                printf("Valores aceitos");
+            }
+        }
+    }
+    else
+    {
+        printf("Valores nao aceitos");
+    }
+    return 0;
+}
