61fb084a841417f722bde48a648245127805bd3c
vkku <kumar.vivekten@gmail.com>
2016-09-07 21:19:03 +0530

    URI 1045

---
+++ Untitled2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a = 5, b = 5, c = 5;
+    if((a == b) && (b == c))
+    {
+        printf("Done\n");
+    }
+    return 0;
+}
---
+++ triangleTypes_1045-GreaterToSmallestOfThree.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    float a, b, c;
+    float g1, g2, g3;
+    scanf("%f %f %f",&a,&b,&c);
+    if(a > b)
+    {
+        if(a > c)
+        {
+            if(b > c)
+            {
+                g1 = a;
+                g2 = b;
+                g3 = c;
+            }
+            else
+            {
+                g1 = a;
+                g2 = c;
+                g3 = b;
+            }
+        }
+        else
+        {
+            g1 = c;
+            g2 = a;
+            g3 = b;
+        }
+    }
+    else if(b > c)
+    {
+        if(a > c)
+        {
+             g1 = b;
+             g2 = a;
+             g3 = c;
+        }
+        else
+        {
+            g1 = b;
+            g2 = c;
+            g3 = a;
+        }
+    }
+    else
+    {
+        g1 = c;
+        g2 = b;
+        g3 = a;
+    }
+
+    if(g1 >= g2 + g3)
+    {
+        printf("NAO FORMA TRIANGULO\n");
+    }
+    if((g1 * g1) == (g2 * g2) + (g3 * g3))
+    {
+        printf("TRIANGULO RETANGULO\n");
+    }
+    if((g1 * g1) > ((g2 * g2) + (g3 * g3)))
+    {
+        printf("TRIANGULO OBTUSANGULO\n");
+    }
+    if((g1 * g1) < (g2 * g2) + (g3 * g3))
+    {
+        printf("TRIANGULO ACUTANGULO\n");
+    }
+    if((abs(g1 - g2) < 0.001 ) && ((abs(g2 - g3) < 0.001)))
+    {
+        printf("TRIANGULO EQUILATERO\n");
+    }
+    if((abs(g1 - g2) < 0.001 ) && (abs(g3 - g1) > 0.001 ) )
+    {
+        printf("TRIANGULO ISOSCELES\n");
+    }
+    if((abs(g2 - g3) < 0.001) && (abs(g1 - g2) > 0.001))
+    {
+        printf("TRIANGULO ISOSCELES\n");
+    }
+    if((abs(g3 - g1) < 0.001) && (abs(g2 - g3) > 0.001))
+    {
+        printf("TRIANGULO ISOSCELES\n");
+    }
+    return 0;
+}
